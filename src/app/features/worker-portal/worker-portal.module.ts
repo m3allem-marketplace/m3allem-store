4dd2575975faad7a42e748a0d7925978059d0909
omaryoussef160 <omaryoussef150@gmail.com>
2026-06-22 22:56:42 +0300

    TASK-E2-12: wire worker portal module

--- src/app/features/worker-portal/worker-portal.module.ts
+++ src/app/features/worker-portal/worker-portal.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { WorkerPortalRoutingModule } from './worker-portal-routing.module';
+import { SharedModule } from '../../shared/shared.module';
+
+import { PortalHomeComponent } from './pages/portal-home/portal-home.component';
+import { JobDetailsComponent } from './pages/job-details/job-details.component';
 
 @NgModule({
-  imports: [CommonModule]
+  declarations: [
+    PortalHomeComponent,
+    JobDetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    WorkerPortalRoutingModule,
+    SharedModule
+  ]
 })
 export class WorkerPortalModule {}
